d88a294a062302fba2bd404ec4433c6bfed78848
Glinfen <gilinfen@gmail.com>
2023-09-15 13:04:16 +0800

    perf: ⚡️ 添加 docker

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM node:alpine AS builder
+
+# 设置淘宝镜像
+RUN npm config set registry https://registry.npm.taobao.org
+
+WORKDIR /app
+COPY . .
+
+# 安装cross-env
+RUN npm install -g cross-env
+
+RUN cd ./web && npm install && npm run build
+
+FROM alpine:latest
+
+RUN apk update && apk add nginx && rm -rf /var/cache/apk/*
+
+WORKDIR /app
+
+EXPOSE 80
+
+COPY --from=builder /app/web/dist /usr/share/nginx/dist
+
+COPY --from=builder /app/nginx.conf /etc/nginx/http.d/default.conf
+
+CMD ["nginx", "-g", "daemon off;"]
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import qiankun from 'vite-plugin-qiankun'
+import compression from 'vite-plugin-compression'
 
 const env = process.env.VITE_ENV
 
@@ -11,7 +12,8 @@ export default defineConfig({
     env === 'prod' ? react() : void 0,
     qiankun(packageName, {
       useDevMode: true
-    })
-  ],
-  base: env === 'dev' ? 'http://127.0.0.1:5500/' : `/${packageName}/`
+    }),
+    compression()
+  ]
+  // base: env === 'dev' ? 'http://127.0.0.1:5500/' : `/${packageName}/`
 })
